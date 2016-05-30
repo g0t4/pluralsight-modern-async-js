d0b62f516f62ed592bf04102fb827d5bdde7e296
Wes Higbee <wes.mcclure@gmail.com>
2016-05-30 16:04:31 -0400

    Bypass error handler if there is no error, just forward success result to proxy promise

--- src/operation.js
+++ src/operation.js
@@ -102,6 +102,7 @@ function Operation() {
           callbackResult.forwardCompletion(proxyOp);
         }
       }
+      else proxyOp.succeed(operation.result);
     }
 
     function errorHandler() {
