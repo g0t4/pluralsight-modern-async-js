b8f24fc808a01491081011cac9a344df98539239
Wes Higbee <wes.mcclure@gmail.com>
2016-06-03 14:15:19 -0400

    Parallel ops and error handling with Async/Await

--- src/asyncawait.js
+++ src/asyncawait.js
@@ -3,17 +3,18 @@ const appid = 'bad6f8a83fe5c5b46cf478d12a8c638c';
 const weatherUrl = `http://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${appid}&units=imperial`;
 const fiveDayUrl = `http://api.openweathermap.org/data/2.5/forecast?q=${city}&appid=${appid}&units=imperial`;
 
-me();
+me()
+  .catch(e => console.log(`error: ${e}`))
 
 async function me() {
   console.log("loading...");
 
-  const weatherResponse = await fetch(weatherUrl);
-  const weather = await weatherResponse.json();
+  const weatherRequest = fetch(weatherUrl).then(r => r.json());
+  const fiveDayRequest = fetch(fiveDayUrl).then(r => r.json());
 
-  const fiveDayResponse = await fetch(fiveDayUrl);
-  const fiveDay = await fiveDayResponse.json();
+  const weather = await weatherRequest;
+  const fiveDay = await fiveDayRequest;
 
   console.log(weather);
   console.log(fiveDay);
-} 
+}
